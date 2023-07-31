31c94f36d3555c8eef86f643d9874d0b6ba4d26c
Crowdbotics <team@crowdbotics.com>
2023-07-31 15:18:46 +0000

    Initial commit.

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "findhandy-42543",
+  url: "https://findhandy-42543.botics.co",
+  api: "https://findhandy-42543.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
